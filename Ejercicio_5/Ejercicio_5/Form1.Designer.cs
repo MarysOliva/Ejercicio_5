9513e1031517c3832dc4d2565bee9d84128bbed2
MarysOliva <86396398+MarysOliva@users.noreply.github.com>
2021-07-02 11:54:38 -0600

    commit número 1
    Ejercicio_5  matriz con numeros aleatorios

---
+++ Ejercicio_5/Ejercicio_5/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Ejercicio_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_matz = new System.Windows.Forms.TextBox();
+            this.txt_num = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_generar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txt_matz
+            // 
+            this.txt_matz.Location = new System.Drawing.Point(295, 27);
+            this.txt_matz.Name = "txt_matz";
+            this.txt_matz.Size = new System.Drawing.Size(100, 20);
+            this.txt_matz.TabIndex = 0;
+            // 
+            // txt_num
+            // 
+            this.txt_num.Location = new System.Drawing.Point(295, 59);
+            this.txt_num.Name = "txt_num";
+            this.txt_num.Size = new System.Drawing.Size(100, 20);
+            this.txt_num.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Purple;
+            this.label1.Location = new System.Drawing.Point(15, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = " Ingrese tamaño de la matriz----------------";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Purple;
+            this.label2.Location = new System.Drawing.Point(17, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(277, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numero-------------------------------------------";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(69, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(334, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "MATRIZ CON NUMEROS ALEATORIOS ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txt_matz);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txt_num);
+            this.groupBox1.Location = new System.Drawing.Point(31, 73);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(408, 100);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // btn_generar
+            // 
+            this.btn_generar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_generar.Location = new System.Drawing.Point(320, 179);
+            this.btn_generar.Name = "btn_generar";
+            this.btn_generar.Size = new System.Drawing.Size(100, 30);
+            this.btn_generar.TabIndex = 4;
+            this.btn_generar.Text = "Generar";
+            this.btn_generar.UseVisualStyleBackColor = true;
+            this.btn_generar.Click += new System.EventHandler(this.btn_generar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(480, 235);
+            this.Controls.Add(this.btn_generar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_matz;
+        private System.Windows.Forms.TextBox txt_num;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_generar;
+    }
+}
+
---
+++ Ejercicio_5/Ejercicio_5/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_generar_Click(object sender, EventArgs e)
+        {
+            int[,] matriz = new int[Convert.ToInt32(txt_num.Text), Convert.ToInt32(txt_matz.Text)];
+            Random matrizaleatoria = new Random();
+
+        
+           for (int fila = 0; fila<matriz.GetLength(0) ; fila++)
+            {
+                for (int columna=0; columna<matriz.GetLength(1); columna++)
+                { matriz[fila, columna] = matrizaleatoria.Next(0, 100);
+                    MessageBox.Show("la posicion es:" + fila + "," + columna + "=" + matriz[fila, columna].ToString());
+                }
+            }
+        }
+    }
+
+
+
+
+}
+
+
+
+
